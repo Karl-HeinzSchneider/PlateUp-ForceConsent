e3832f1249d5c8c61e01f3a8e35b1a74856c1056
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2024-05-03 09:06:55 +0200

    Dev (#1)
    * ForcePractice
    
    * Add Helper
    
    * Update ForcePractice.cs
    
    * ForceLoad
    
    * ForceSave
    
    * Add plateup mod

--- Helper.cs
+++ Helper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Kitchen;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,22 @@ namespace PlateUp_ForceConsent
 {
     public static class Helper
     {
+
+        public static int GetLocalPlayerID()
+        {
+            int LocalID = 0;
+
+            foreach (PlayerInfo info in Players.Main.All())
+            {
+                if (info.IsLocalUser)
+                {
+                    //this.LogObject(info);
+                    LocalID = info.ID;
+                    break;
+                }
+            }
+
+            return LocalID;
+        }
     }
 }
