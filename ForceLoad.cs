bf1b1f6bd949bd1abf985ba0a8e2f765e8d274fd
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2024-05-03 07:48:57 +0200

    ForceLoad

---
+++ ForceLoad.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Reflection;
+
+using Kitchen;
+using KitchenMods;
+
+using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.Entities;
+using Unity.Collections;
+
+using Kitchen.Modules;
+
+namespace PlateUp_ForceConsent
+{
+    public class ForceLoad : GenericSystemBase, IModSystem
+    {
+        int LocalID = 0;  
+
+        protected override void Initialise()
+        {
+            base.Initialise();
+            Debug.Log("ForceLoad");
+            RequireSingletonForUpdate<SFranchiseMarker>();             
+        }
+
+        protected override void OnUpdate()
+        {
+            if(this.LocalID == 0)
+            {
+                this.LocalID = Helper.GetLocalPlayerID();
+            }
+
+            ConsentElement[] consentElements = GameObject.FindObjectsOfType<ConsentElement>();
+
+            if(consentElements.Length < 2 )
+            {
+                //Debug.Log("consentElements.Length < 2");
+                return;
+            }
+
+            ConsentElement elem = consentElements[1];
+
+            if (elem.GetConsent(this.LocalID))
+            {
+                elem.SetAllConsents(true);
+            }
+        }
+    }
+}
